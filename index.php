073da759b2f32539102faa66527403bdbe3b4b8a
MrManGuyMan <113818758+MrManGuyMan@users.noreply.github.com>
2025-03-04 15:05:13 +1100

    initial commit

--- index.php
+++ index.php
@@ -142,7 +142,7 @@ $total_pages = ceil($total_reviews / $limit);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Reviews - Add & View Reviews</title>
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="/assets/css/all.min.css">
 </head>
 <body>
 <header>
